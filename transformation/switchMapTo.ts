c6764079cd2fac211411a6b805d817e4981e0e12
panicoenlaxbox <panicoenlaxbox@gmail.com>
2019-11-06 18:37:41 +0100

    switchMapTo use case

--- transformation/switchMapTo.ts
+++ transformation/switchMapTo.ts
@@ -1,6 +1,9 @@
 import { of, interval } from 'rxjs';
 import { switchMapTo } from 'rxjs/operators';
 
+// caso de uso
+//  como los de switchMap pero no me importa la entrada, por ejemplo, me suscribo a un evento de click y quiero hacer una llamada a la API con valor constante
+//      o cogiendo el valor de otro sitio que no es observable source/outer
 const obs1 = interval(500).pipe(
     switchMapTo(of(1))
 );
